b4615990fddcb1d8cd4c58b3b8c41e44d1399685
SAMSUNG <wlsdmsrnfl@naver.com>
2022-01-24 20:10:40 +0900

    scanner 활용한 경우

---
+++ Practice/While/While_2.java
@@ -0,0 +1,15 @@
+package While;
+import java.util.*;
+
+public class While_2 {
+    public static void main(String[] args) throws Exception  {
+
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a+b);
+
+        }
+    }
+}
