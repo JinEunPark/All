2947e1d92f3ad539e9e0d4fcdff3a0e227c23e4c
SAMSUNG <wlsdmsrnfl@naver.com>
2022-01-26 16:42:42 +0900

    String to make AXCII code

---
+++ Practice/String_practice/String_1.java
@@ -0,0 +1,13 @@
+package String_practice;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class String_1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println((int) (str.charAt(0)));
+    }
+}
