1e97a71b567211eefce37cc770ff1a905a6e9b72
SAMSUNG <wlsdmsrnfl@naver.com>
2022-01-29 13:59:22 +0900

    연속되지 않는 단어를 거르는 방법 하나의 단어를 차례대로 선택하고 같은 문자가 단어 내에 존재하는 경우에 same이라는 상수를 증가시켜 준 후 이 same = =0 인 경우엔 증가 연속적인 단어라고 취급함.

---
+++ Practice/String_practice/String_11.java
@@ -0,0 +1,48 @@
+package String_practice;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class String_11 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String str[] = new String[n];
+        int count = 0;
+        int same[] = new int[n];
+
+
+        for (int e = 0; e < n; e++) {
+
+            str[e] = br.readLine();
+
+            for (int i = 0; i < str[e].length(); i++) {
+
+                char c = str[e].charAt(i);
+
+                if (i < str[e].length() - 1) {
+
+                    for (int j = i + 1; j < str[e].length(); j++) {
+
+                        char b = str[e].charAt(j);
+
+                        if (c == b && j - i != 1) {
+                            same[e]++;
+                        }
+                    }
+
+                } else continue;
+            }
+
+        }
+        for (int i = 0; i < n; i++) {
+            if (same[i] == 0) {
+                count++;
+            }
+        }
+        System.out.print(count);
+
+    }
+}
