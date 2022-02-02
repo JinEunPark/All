e6037ad993fec679347237d02d66ef56119cfed6
SAMSUNG <wlsdmsrnfl@naver.com>
2022-02-02 16:02:48 +0900

    정수형 범윌ㄹ 벋어나는 숫자의 덧샘

---
+++ Practice/Math_thinking/Math_8.java
@@ -0,0 +1,43 @@
+package Math_thinking;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Math_8 {
+    public static long fly(long start, long destination) {
+        long distacne = destination - start;
+        long max;
+        long count = 0;
+        max = (int) Math.sqrt(distacne);
+        if (max == Math.sqrt(distacne)) {
+            count = max * 2 - 1;
+        } else {
+            if (distacne <= max * max + max) {
+                count = max * 2;
+            } else {
+                count = max * 2 + 1;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            long start = (long) Integer.parseInt(st.nextToken());
+            long destination = (long) Integer.parseInt(st.nextToken());
+
+            sb.append(String.valueOf(fly(start, destination))).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
